264afff2332a57d1600f359e3e74e12fd630d532
Mishulka <misailgromov@gmail.com>
2025-05-12 11:00:26 +0300

    init

---
+++ index.js
@@ -0,0 +1,42 @@
+//? imports
+import { add } from './src/algo/example.js';
+
+
+//!example.js || adding 2 numbers
+
+// const x = add(1, 2);
+// console.log(x);
+
+
+//! 2
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
